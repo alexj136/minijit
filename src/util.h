3d7d730efec744ba1befb4e5f4c4505d19fbf959
Alex Jeffery <apjeffery136@googlemail.com>
2014-12-03 17:46:25 +0000

    Began implementing generic vectors

--- src/util.h
+++ src/util.h
@@ -1,12 +1,44 @@
 #ifndef util
 #define util
 
-#define RefVectorable(ty) \
+#define VECTOR_INITIAL_ARR_SIZE 10
+
+#define DEFINE_VECTORABLE(ty) \
+    \
+    /* Define a Vector type containing elements of type 'ty'. */ \
     typedef struct ty##Vector ty##Vector; \
     struct ty##Vector { \
-        int length; \
-        ty **arr; \
-    };
+        /* number of elements */        int size \
+        /* size of underlying array */  int arr_size; \
+        /* pointer to array*/           ty **arr; \
+    }; \
+    \
+    /* Initialise a Vector */ \
+    ty##Vector **ty##Vector_init() { \
+        ty##Vector *vec = challoc(sizeof(ty##Vector)); \
+        vec->size = 0; \
+        vec->arr_size = VECTOR_INITIAL_ARR_SIZE; \
+        vec->arr = challoc(sizeof(ty *) * size); \
+        return vec; \
+    } \
+    \
+    /* Determine the size (number of elements) of a Vector */ \
+    int ty##Vector_size(ty##Vector *vec) { \
+        return vec->size; \
+    } \
+    \
+    /* Append a new element of type 'ty' to a Vector of elements of that */ \
+    /* type. */ \
+    void ty##Vector_append(ty##Vector *vec, ty *elem) { \
+        if(vec->size >= vec->arr_size) { \
+            vec->arr = \
+                    chrealloc(vec->arr, sizeof(ty *) * (vec->arr_size * 2)); \
+            vec->arr_size *= 2; \
+        } \
+        vec->arr[vec->size] = elem; \
+        vec->size++; \
+    } \
+
 
 void *challoc(int size);
 
