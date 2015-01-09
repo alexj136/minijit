9a16eeb07de59373f25f5be43f42b5736f422662
Alex Jeffery <apjeffery136@googlemail.com>
2015-01-09 17:29:18 +0000

    Began refactoring lexer to take strings as arguments as well as files.

--- test/test_lexer.c
+++ test/test_lexer.c
@@ -1,4 +1,11 @@
 #include "minunit.h"
+#include "lexer.h"
 
 MINUNIT_TESTS
+
+    TEST("")
+        char *text = "while a - 1 do {return a}";
+        LexerResult *lr = lex_file(text);
+    END
+
 END_TESTS
