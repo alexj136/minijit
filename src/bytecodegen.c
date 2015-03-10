83129c0a8a3a62809b38d11ac7f24c5f8e5268d2
Alex Jeffery <apjeffery136@googlemail.com>
2015-03-10 14:18:06 +0000

    Continued bytecode interpreter

--- src/bytecodegen.c
+++ src/bytecodegen.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
 #include "bytecodegen.h"
+
+/*
+ * Operation functions
+ */
+
+Operation *Operation_init(Opcode opc, int arg1, int arg2) {
+    Operation *op = challoc(sizeof(Operation));
+    op->opc       = opc;
+    op->arg1      = arg1;
+    op->arg2      = arg2;
+    return op;
+}
+
+bool Operation_eq(Operation *op1, Operation *op2) {
+    // Null checks are essentially to supress the unused parameter warning
+    if(!op1) { ERROR("Null Operation"); }
+    if(!op2) { ERROR("Null Operation"); }
+    ERROR("Equality not defined for Operation");
+}
+
+void Operation_free(Operation *op) { free(op); }
+
+DEFINE_VECTORABLE(Operation)
--- src/bytecodegen.h
+++ src/bytecodegen.h
@@ -5,4 +5,44 @@
 #ifndef bytecodegen
 #define bytecodegen
 
+/*
+ * Register names
+ */
+
+#define STACK_POINTER        (-4)
+#define FRAME_POINTER        (-3)
+#define RETURN_ADDRESS       (-2)
+#define PROGRAM_COUNTER      (-1)
+#define GENERAL_PURPOSE(num) (num)
+
+/*
+ * Operation structs - intermediate level representation
+ */
+
+typedef enum
+    { LOAD
+    , STORE
+    , ADD
+    , SUB
+    , LOADIMM
+    , JUMP
+    , JUMPIF0
+    , JUMPLINK
+    , LABEL
+    , HALT
+    } Opcode;
+
+typedef struct Operation Operation;
+struct Operation {
+    Opcode opc;
+    int arg1;
+    int arg2;
+};
+
+Operation *Operation_init(Opcode opc, int arg1, int arg2);
+bool Operation_eq(Operation *op1, Operation *op2);
+void Operation_free(Operation *op);
+
+FORWARD_DECLARE_VECTORABLE(Operation)
+
 #endif // bytecodegen
