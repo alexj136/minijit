05c383113201764f42fcf2c927e61cbf092a7d57
Alex Jeffery <apjeffery136@googlemail.com>
2015-04-15 09:47:57 +0100

    Continued native code generator

--- src/ncode.h
+++ src/ncode.h
@@ -1,6 +1,19 @@
 #ifndef ncode
 #define ncode
 
+/*
+ * Translate the icode HALT instruction to x86_64.
+ */
+#define HALT_to_x86_64 \
+    \
+    /* Some operation to save the accumulator value */ \
+    \
+    /* movq $60, %rax ; Put $60 exit syscall code in correct register */ \
+    0x48, 0xc7, 0xc0, 0x3c, 0x00, 0x00, 0x00, \
+    \
+    /* Do the syscall */ \
+    x86_64_syscall
+
 #define x86_64_syscall 0x0F, 0x05
 
 #endif // ncode
