f62cedfa8e16163da99fa2a3246dbcfeb4d97d9f
Alex Jeffery <apjeffery136@googlemail.com>
2015-04-17 10:13:45 +0100

    Continued native code generator - implemented register translation table

--- src/ncode.h
+++ src/ncode.h
@@ -1,33 +1,15 @@
 #ifndef ncode
 #define ncode
 
-typedef byte unsigned char
+typedef unsigned char byte;
 
-#define EAX_EAX 0xc0
-#define EAX_EBX 0xc3
-#define EAX_ECX 0xc1
-#define EAX_EDX 0xc2
-
-#define EBX_EAX 0xd8
-#define EBX_EBX 0xdb
-#define EBX_ECX 0xd9
-#define EBX_EDX 0xda
-
-#define ECX_EAX 0xc8
-#define ECX_EBX 0xcb
-#define ECX_ECX 0xc9
-#define ECX_EDX 0xca
-
-#define EDX_EAX 0xd0
-#define EDX_EBX 0xd3
-#define EDX_ECX 0xd1
-#define EDX_EDX 0xd2
-
-#define reg_to_x86_64 \
+#define ADD_to_x86_64(r1, r2) \
     \
+    0x01, reg_pair_code(r1, r2)
 
-#define ADD_to_x86_64 \
+#define SUB_to_x86_64(r1, r2) \
     \
+    0x29, reg_pair_code(r1, r2)
 
 /*
  * Translate the icode HALT instruction to x86_64.
@@ -44,8 +26,27 @@ typedef byte unsigned char
 
 #define x86_64_syscall 0x0F, 0x05
 
-byte reg_pair_code(byte r1, byte r2) {
+byte reg_pair_code(int r1, int r2) {
+    if((r1 > TEMPORARY) || (r1 < STACK_POINTER) ||
+            (r2 > TEMPORARY) || (r2 < STACK_POINTER)) {
 
+        ERROR("Invalid register");
+    }
+    else if((r1 == PROGRAM_COUNTER) || (r2 == PROGRAM_COUNTER)) {
+        ERROR("Cannot modify or use program counter register");
+    }
+    else { return reg_lookup_table[r1][r2]; }
 }
+
+byte reg_lookup_table[6][6] = {
+    /* (Virtual)    SP      FP      RA      PC      ACC     TMP */
+    /* (Native)     ESP     EBP     ECX     EIP     EAX     EBX */
+    /* SP  ESP */ { 0xE4,   0xEC,   0xCC,   0x00,   0xC4,   0xDC },
+    /* FP  EBP */ { 0xE5,   0xED,   0xCD,   0x00,   0xC5,   0xDD },
+    /* RA  ECX */ { 0xE1,   0xE9,   0xC9,   0x00,   0xC1,   0xD9 },
+    /* PC  EIP */ { 0x00,   0x00,   0x00,   0x00,   0x00,   0x00 },
+    /* ACC EAX */ { 0xE0,   0xE8,   0xC8,   0x00,   0xC0,   0xD8 },
+    /* TMP EBX */ { 0xE3,   0xEB,   0xCB,   0x00,   0xC3,   0xDB }
+};
 
 #endif // ncode
