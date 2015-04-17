4d131e7a814e9e7fa758c8d1890ac995f5cf350b
Alex Jeffery <apjeffery136@googlemail.com>
2015-04-17 12:54:58 +0100

    Implemented loadimm as native

--- src/ncode.c
+++ src/ncode.c
@@ -4,7 +4,7 @@
 #include "icode.h"
 #include "ncode.h"
 
-byte reg_lookup_table[6][6] = {
+byte dual_reg_lookup_table[6][6] = {
     /* (Virtual)    SP      FP      RA      PC      ACC     TMP */
     /* (Native)     ESP     EBP     ECX     EIP     EAX     EBX */
     /* SP  ESP */ { 0xE4,   0xEC,   0xCC,   0x00,   0xC4,   0xDC },
@@ -15,7 +15,13 @@ byte reg_lookup_table[6][6] = {
     /* TMP EBX */ { 0xE3,   0xEB,   0xCB,   0x00,   0xC3,   0xDB }
 };
 
-byte reg_pair_code(int r1, int r2) {
+byte single_reg_lookup_table[6] = {
+    /* (Virtual)    SP      FP      RA      PC      ACC     TMP */
+    /* (Native)     ESP     EBP     ECX     EIP     EAX     EBX */
+                    0xBC,   0xBD,   0xB9,   0x00,   0xB8,   0xBB
+};
+
+byte dual_reg_to_x86_64(int r1, int r2) {
     if((r1 > TEMPORARY) || (r1 < STACK_POINTER) ||
             (r2 > TEMPORARY) || (r2 < STACK_POINTER)) {
 
@@ -24,5 +30,15 @@ byte reg_pair_code(int r1, int r2) {
     else if((r1 == PROGRAM_COUNTER) || (r2 == PROGRAM_COUNTER)) {
         ERROR("Cannot modify or use program counter register");
     }
-    else { return reg_lookup_table[r1][r2]; }
+    else { return dual_reg_lookup_table[r1][r2]; }
+}
+
+byte single_reg_to_x86_64(int r) {
+    if((r > TEMPORARY) || (r < STACK_POINTER)) {
+        ERROR("Invalid register");
+    }
+    else if(r == PROGRAM_COUNTER) {
+        ERROR("Cannot modify or use program counter register");
+    }
+    else { return single_reg_lookup_table[r]; }
 }
